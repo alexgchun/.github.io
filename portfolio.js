361bbfce4afe18e0b8a5ba68c5a0e3bdb23241b6
alexgchun <90280800+alexgchun@users.noreply.github.com>
2021-12-14 09:11:00 -0800

    update

--- portfolio.js
+++ portfolio.js
@@ -1,11 +1,12 @@
 // These functions open and close the contact form
 function openForm() {
     document.getElementById("myForm").style.display = "block";
-}
+}//formpopup class, display non at first
+//this makes it appear
 
 function closeForm() {
     document.getElementById("myForm").style.display = "none";
-}
+}//back to none 
 
 // This function displays first image in the slideshow when the page loads
 var slideIndex = 1;
@@ -26,13 +27,22 @@ function showSlides(n) {
     var slides = document.getElementsByClassName("mySlides"); // This takes all elements with the class name "mySlides" and stores them in the variable array "slides"
     var dots = document.getElementsByClassName("dot"); // This takes all elements with the class name "dot" and stores them in the variable array "dots"
     if (n > slides.length) {slideIndex = 1}; // If n (the number passed into the function) is greater than the length of the array "slides", the slideIndex is set to 1
+    //i think this is for the plusslides function
     if (n < 1) {slideIndex = slides.length}; // If n (the number passed into the function) is less than 1, te slideIndex is set to the length of the array "slides"
+    //ithink this is for the plusslides function
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none"; // This for loop takes each item in the array "slides" and sets the display to none
     }
+    //iterated through all slides and sets display to none
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", ""); // This for loop takes each item in the array "dots" and removes "active" which removes the active styling
     }
+    //removes any active styling given in line 47 or line 12
+
+    //arrayorspans[i].className = ".style"| .className give class to the element
+    //give span tags class of 
+    //for the array, replace the active styling to null
+
     slides[slideIndex - 1].style.display = "block"; // This displays the image in the slideshow
     dots[slideIndex - 1].className += " active"; // This adds the active styling to the dot associated with the image
 }
